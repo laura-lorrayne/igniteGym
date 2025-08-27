ccab17226297445bb92b7b4826d4eb7c76aa808d
Laura-lorrayne <lauralorrayne24@gmail.com>
2025-08-27 11:42:48 -0300

    criando rotas

--- App.tsx
+++ App.tsx
@@ -7,8 +7,7 @@ import {
 import { GluestackUIProvider, Text, Center } from "@gluestack-ui/themed";
 import { config } from "./config/gluestack-ui.config";
 import { Loading } from "@components/Loading";
-import { SignIn } from "@screens/Signin";
-import { SignUp } from "@screens/SignUp";
+import { Routes } from "@routes/index";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -23,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <SignUp /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </GluestackUIProvider>
   );
 }
--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,4 +1,11 @@
-import { VStack, Image, Center, Text, Heading } from "@gluestack-ui/themed";
+import {
+  VStack,
+  Image,
+  Center,
+  Text,
+  Heading,
+  ScrollView,
+} from "@gluestack-ui/themed";
 import BackgroundImg from "@assets/background.png";
 import Logo from "@assets/logo.svg";
 import { Input } from "@components/Input";
@@ -6,44 +13,49 @@ import { Button } from "@components/Button";
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="$gray700">
-      <Image
-        w="$full"
-        h={624}
-        source={BackgroundImg}
-        defaultSource={BackgroundImg}
-        alt="pessoas treinando"
-        position="absolute"
-      />
-      <VStack flex={1} px="$10" pt="$16">
-        <Center my="$24">
-          <Logo />
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="pessoas treinando"
+          position="absolute"
+        />
+        <VStack flex={1} px="$10" pt="$16">
+          <Center my="$24">
+            <Logo />
 
-          <Text color="$gray100" fontSize="$sm">
-            Treine sua mente e seu corpo
-          </Text>
-        </Center>
+            <Text color="$gray100" fontSize="$sm">
+              Treine sua mente e seu corpo
+            </Text>
+          </Center>
 
-        <Center gap="$2">
-          <Heading color="$gray100">Acesse sua conta</Heading>
+          <Center gap="$2">
+            <Heading color="$gray100">Acesse sua conta</Heading>
 
-          <Input
-            placeholder="Email"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
+            <Input
+              placeholder="Email"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
 
-          <Input placeholder="Senha" secureTextEntry />
-          <Button title="Acessar" />
-        </Center>
+            <Input placeholder="Senha" secureTextEntry />
+            <Button title="Acessar" />
+          </Center>
 
-        <Center flex={1} justifyContent="flex-end" mb="$4">
-          <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
-            Ainda não tem acesso?
-          </Text>
-          <Button title="Criar conta" variant="outline" />
-        </Center>
+          <Center flex={1} justifyContent="flex-end" mb="$4">
+            <Text color="$gray100" fontSize="$sm" mb="$3" fontFamily="$body">
+              Ainda não tem acesso?
+            </Text>
+            <Button title="Criar conta" variant="outline" />
+          </Center>
+        </VStack>
       </VStack>
-    </VStack>
+    </ScrollView>
   );
 }
