f0316e344a332055159013152c7dddea2338b25d
Laura-lorrayne <lauralorrayne24@gmail.com>
2025-09-03 15:52:22 -0300

    criaçao Toast

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,42 +1,73 @@
-import { ScreenHeader } from "@components/ScreenHeader";
-import { UserPhoto } from "@components/UserPhoto";
-import { Center, VStack, Text, Heading } from "@gluestack-ui/themed";
-import { ScrollView, TouchableOpacity } from "react-native";
-import { Input } from "@components/Input";
+/* eslint-disable no-useless-return */
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { ToastMessage } from "@components/ToastMessage";
+import { UserPhoto } from "@components/UserPhoto";
+import { Center, Heading, Text, useToast, VStack } from "@gluestack-ui/themed";
+import * as FileSystem from "expo-file-system";
 import * as ImagePicker from "expo-image-picker";
 import { useState } from "react";
+import { ScrollView, TouchableOpacity } from "react-native";
 
 export function Profile() {
   const [userPhoto, setUserPhoto] = useState(
-    "https://github.com/Laura-lorrayne.png"
+    "https://github.com/arthurrios.png"
   );
 
-  async function handleUserPhotoSelect() {
-    const photoSelected = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.Images,
-      quality: 1,
-      aspect: [4, 4],
-      allowsEditing: true,
-    });
+  const toast = useToast();
 
-    if (photoSelected.canceled) {
-      return;
+  async function handleUserPhotoSelect() {
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4, 4],
+        allowsEditing: true,
+      });
+
+      if (photoSelected.canceled) {
+        return;
+      }
+
+      const photoUri = photoSelected.assets[0].uri;
+
+      if (photoUri) {
+        const photoInfo = (await FileSystem.getInfoAsync(photoUri)) as {
+          size: number;
+        };
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 1) {
+          return toast.show({
+            placement: "top",
+            render: ({ id }) => (
+              <ToastMessage
+                id={id}
+                action="error"
+                title="Essa imagem é muito grande. Escolha uma de até 5MB"
+                onClose={() => toast.close(id)}
+              />
+            ),
+          });
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri);
+      }
+    } catch (error) {
+      console.log(error);
     }
-    setUserPhoto(photoSelected.assets[0].uri);
   }
 
   return (
-    <VStack>
+    <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+
       <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
-        <Center mt={6} px={10}>
+        <Center mt="$6" px="$10">
           <UserPhoto
             source={{ uri: userPhoto }}
-            w="$16"
-            h="$16"
-            alt="Imagem do usuário"
             size="xl"
+            alt="Imagem do usuário"
           />
 
           <TouchableOpacity onPress={handleUserPhotoSelect}>
@@ -44,16 +75,16 @@ export function Profile() {
               color="$green500"
               fontFamily="$heading"
               fontSize="$md"
-              mt={"$2"}
-              mb={"$8"}
+              mt="$2"
+              mb="$8"
             >
               Alterar Foto
             </Text>
           </TouchableOpacity>
 
-          <Center w={"$full"} gap={"$4"}>
+          <Center w="$full" gap="$4">
             <Input placeholder="Nome" bg="$gray600" />
-            <Input placeholder="E-mail" bg="$gray600" isReadOnly />
+            <Input value="arthur@email.com" bg="$gray600" isReadOnly />
           </Center>
 
           <Heading
@@ -61,13 +92,13 @@ export function Profile() {
             fontFamily="$heading"
             color="$gray200"
             fontSize="$md"
-            mt={"$12"}
-            mb={"$2"}
+            mt="$12"
+            mb="$2"
           >
             Alterar senha
           </Heading>
 
-          <Center w={"$full"} gap={"$4"}>
+          <Center w="$full" gap="$4">
             <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
             <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
             <Input
@@ -75,7 +106,8 @@ export function Profile() {
               bg="$gray600"
               secureTextEntry
             />
-            <Button style={{ marginTop: 8 }} title="Atualizar" />
+
+            <Button title="Atualizar" />
           </Center>
         </Center>
       </ScrollView>
